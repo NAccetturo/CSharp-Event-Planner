c3c87334211f467811ce0406c75f1409919846b0
Nicolas Accetturo <naccetturo@gmail.com>
2019-08-26 13:46:51 -0700

    Third Commit, saving changes to MyContext.

--- Models/DataModels/MyContext.cs
+++ Models/DataModels/MyContext.cs
@@ -6,7 +6,6 @@ namespace BeltExam.Models
     {
         // base() calls the parent class' constructor passing the "options" parameter along
         public MyContext(DbContextOptions options) : base(options) { }
-        // "users" table is represented by this DbSet "Users"
         public DbSet<User> Users {get;set;}
         public DbSet<Event> Events {get;set;}
         public DbSet<RSVP> RSVPS {get;set;}
